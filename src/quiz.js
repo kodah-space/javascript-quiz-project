9988be45d8a07ec56a2641b1c4f9360699597179
Shakthi Prasad KT <shakthi.kt@gmail.com>
2024-04-04 17:30:44 +0200

    Day 2 done

--- src/quiz.js
+++ src/quiz.js
@@ -42,4 +42,19 @@ class Quiz {
       return true;
     }
   }
+  filterQuestionsByDifficulty(difficulty) {
+    if (1 <= difficulty && difficulty <= 3) {
+      this.questions = this.questions.filter((question) => {
+        return question.difficulty === difficulty;
+      });
+    } else {
+      return this.questions;
+    }
+  }
+  averageDifficulty() {
+    const totalDifficulty = this.questions.reduce((total, question) => {
+      return total + question.difficulty;
+    }, 0);
+    return totalDifficulty / this.questions.length;
+  }
 }
